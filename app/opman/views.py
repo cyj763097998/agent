a6fea45214f37a47a2feb65045a68595fb9d5dc6
Your Name <you@example.com>
2019-03-27 17:29:52 +0800

    build 19-3-27

--- app/opman/views.py
+++ app/opman/views.py
@@ -3,13 +3,17 @@
 #edit richard  2019/3/14
 
 from . import opman
-#from flask import render_template, url_for, redirect,flash,session,request,abort
+from flask import render_template, url_for, redirect,flash,session,request,abort
 #from forms import LoginForm,PwdForm,TagForm,AuthForm,RoleForm,AdminForm
 #from app.models import Admin,Tag,Auth,Role
 #from functools import wraps
 #from app import db
 #import datetime
-
 @opman.route("/api/create_masterhost",methods=["POST"])
 def create_masterhost():
+    master_dir = request.form.get('master_dir')
+    master_port = request.form.get('master_port')
+    print 11111111111
+    print master_dir
+    print master_port
     return "create_masterhost"
